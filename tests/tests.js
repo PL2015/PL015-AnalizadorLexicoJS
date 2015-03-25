e5cb9fc64ebf0d8f0f892f02ca3297413a8a60a6
Wyllman <wyllman@gmail.com>
2015-03-25 22:16:52 +0000

    Añadiendo archivos de configuracion. Hasta las pruebas con mocha y chai. Creando la base de la web para los tests.

---
+++ tests/tests.js
@@ -0,0 +1,26 @@
+var assert = chai.assert;
+
+
+suite('analizadorLex', function () {
+   test ('1', function () {
+   
+   });
+   
+   test ('2', function () {
+   
+   });
+   
+   
+   test ('3', function () {
+   
+   });
+   
+   
+   test ('4', function () {
+   
+   });
+
+
+
+
+});
